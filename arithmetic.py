0c20948c0f7d64830c8efa7302467c68f73bc531
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 12:19:24 -0700

    Added all math formulas to arithmetic and made calculator work

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def subtract(num1, num2):
+    diff = num1 - num2
+    return diff
+
+def multiply(num1, num2):
+    product = num1*num2
+    return product
+
+def divide(num1, num2):
+    quotient = num1/num2
+    return quotient
+
+def square(num1):
+    squared = num1 * num1
+    return squared
+
+def cube(num1):
+    cubed = num1 * num1 * num1
+    return cubed
+
+def power(num1, num2):
+    powered = num1 ** num2
+    return powered
+
+def mod(num1, num2):
+    modu= num1%num2
+    return modu
