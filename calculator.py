f2d214d4171a9749f15f07ab12fd0d00e5c3b456
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 17:59:07 -0700

    finished the calculator

--- calculator.py
+++ calculator.py
@@ -1,36 +1,32 @@
 from arithmetic import *
 
-
-equation = raw_input("> ")
-
 #math_dictionary = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
-equation_list = equation.split(' ')
-
-
-math_dictionary = {'+': add}
-
+#equation_list = equation.split(' ')
+#equation = raw_input("> ")
 
 def tokenize_equation(equation):
-    #equation_list = equation.split(' ')
-    print equation_list
+    #equation = raw_input("> ")
+    equation_list = equation.split(' ')
+    math_dictionary = {'+': add, '-': subtract, '*': multiply, '/': divide, 
+                        'square': square, 'cube': cube, 'pow': power, 'mod': mod}
     if equation == "q" or equation == "Q":
         quit()
     else:
         if 2 > len(equation_list) > 3:
-            print "I don't understand" 
+            print "I don't understand"
         elif equation_list[0] not in math_dictionary:
             print "I don't understand"
         elif equation_list[0] in math_dictionary.keys():
-            num1 = equation_list[1]
-            num2 = equation_list[2]
-            num1 = int(num1)
-            num2 = int(num2)
-            return math_dictionary[equation_list[0]](num1, num2)
-        
+            num1 = int(equation_list[1])
+            if len(equation_list) == 2:
+                return math_dictionary[equation_list[0]](num1)
+            else:
+                num2 = int(equation_list[2])
+                return math_dictionary[equation_list[0]](num1, num2)
 
-
-
-print tokenize_equation(equation)
+            
+while True:
+    print tokenize_equation(raw_input("> "))
 
 
     
